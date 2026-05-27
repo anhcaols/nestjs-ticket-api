d51f0524b01ff4ee1bc45cadeecedab80ce860db
anhct <anhct@ajna.vc>
2026-05-27 09:08:57 +0700

    delete files

--- src/user/dto/login-user.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty, MinLength } from 'class-validator';
-
-export class LoginUserDto {
-  @IsNotEmpty({ message: 'Account name is required' })
-  accountname: string;
-  @IsNotEmpty({ message: 'Password is required' })
-  @MinLength(8, { message: 'Password must be at least 8 characters long' })
-  password: string;
-}
--- src/user/dto/register-user.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsNotEmpty } from 'class-validator';
-
-export class RegisterUserDto {
-  @IsNotEmpty({ message: 'Account name is required' })
-  accountname: string;
-  @IsNotEmpty({ message: 'Password is required' })
-  password: string;
-}
